fafb187aaf9a58cc76d3061ae177d42013edd633
MelodyChapfugama <chapfugama@gmail.com>
2019-02-28 10:54:10 +0200

    external javascript file
    created an external javascript script and linked it to the html sheet

---
+++ maths.js
@@ -0,0 +1,23 @@
+
+    var x=5
+    var y=6
+
+
+    function add(){
+      window.alert(x+y)
+    }
+    function multiply(){
+      window.alert(x*y)
+    }
+    function divide(){
+      window.alert(x/y)
+    }
+    function subtract(){
+      window.alert(x-y)
+    }
+/*
+    document.getElementById("demo").innerHTML= x+y;
+    document.getElementById("demo").innerHTML= x*y;
+    document.getElementById("demo").innerHTML= x/y;
+    document.getElementById("demo").innerHTML= x-y;
+*/
