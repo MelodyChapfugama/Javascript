3bcb6ededa192f7a423c741659cb099a09fe64e7
MelodyChapfugama <chapfugama@gmail.com>
2019-03-06 11:36:40 +0200

    for, do , while loop

---
+++ fibonacci.js
@@ -0,0 +1,21 @@
+
+
+var text = "" ;
+for (var i = 1; i <= 35 ; i ++){
+  text += i + " " + "<br/>";
+}
+document.write(text);
+
+var numbers = []
+for (var i = 1; i <= 35 ; i ++){
+  numbers.push(i);
+}
+document.write(numbers);
+
+var text =" ";
+var 1 = 0;
+while(i<35){
+  text += i + " "
+  i++
+}
+document.write(text);
