bab9bddce62b5f8b55e73e76748ca1082194f42e
MelodyChapfugama <chapfugama@gmail.com>
2019-03-07 08:54:18 +0200

    if condition

---
+++ ifcondition.js
@@ -0,0 +1,19 @@
+var days = ["mon", "tue", "wed"];
+  if (days[0]=="mon"){
+    document.getElementById("demo").innerHTML = "hey i found monday";
+  } else {
+    document.getElementById("demo").innerHTML = "bummer man, no monday here";
+  }
+
+  if (days[1]=="tue"){
+    document.getElementById("demo1").innerHTML = "hey i found tuesday";
+  } else {
+    document.getElementById("demo1").innerHTML = "bummer man, no tue here";
+  }
+
+
+  if (days[2]=="wed"){
+    document.getElementById("demo2").innerHTML = "hey i found wednesday";
+  } else {
+    document.getElementById("demo2").innerHTML = "bummer man, no wed here";
+  }
